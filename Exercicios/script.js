188a5a695bbc6b5e35f6ab85f7b3f0e163c8b2bf
Ismael Mizael <ismaelchaves2001@outlook.com>
2026-02-25 17:05:23 -0300

    Exercicio Calculo Viagem

---
+++ Exercicios/script.js
@@ -0,0 +1,17 @@
+/*
+Desenvolva um programa em JavaScript que calcule o valor total gasto em uma viagem com base nas seguintes informações:
+
+- Valor da gasolina (por litro)
+- Consumo médio do veículo (km por litro)
+- Distância percorrida (em km)
+*/
+
+function ValorViagem() {
+    let ValorGasolina = document.getElementById('gasolina').value;
+    let ConsumoMedio = document.getElementById('consumo').value;
+    let DistanciaPercorrida = document.getElementById('distancia').value;
+
+    let QuilometrosPorLitro = DistanciaPercorrida / ConsumoMedio;
+    let ValorTotal = QuilometrosPorLitro * ValorGasolina
+    alert('O Valor total da viagem é de $' + ValorTotal)
+}
